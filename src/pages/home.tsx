87b7e0925e5c5e30427f07b6901d4edec1022075
Virqy Apriyadi Jihad <86461430+kikok456@users.noreply.github.com>
2026-05-19 14:41:00 +0700

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,15 +2,19 @@ import Content from "../componen/content";
 
 type Props = {
   isLogin: boolean;
+  status: string;
 };
 
 
-export default function Home({ isLogin }: Props) {
+export default function Home({ isLogin, status, }: Props) {
   return (
     <>
       <div className="container-2">
         <p className="container-2-text">
-          <Content isLogin={isLogin} />
+          <Content
+            isLogin={isLogin}
+            status={status}
+          />
         </p>
       </div>
     </>
